5db066c87e96fba7732e1808e3c3d00d32a7ad26
ShaolinmatadorPrateado <leonardogmcorrea@gmail.com>
2024-02-07 22:26:29 -0300

    TEstfi

---
+++ Migrations/20240208012236_Pessoa2BD.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteFi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pessoa2BD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "SuperPessoa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Celular = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperPessoa", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperPessoa");
+        }
+    }
+}
---
+++ Model/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteFi.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [MaxLength(15)]
+        public string Celular { get; set; }
+
+
+    }
+}
